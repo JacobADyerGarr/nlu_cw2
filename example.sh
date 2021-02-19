9cf1e05ffd0126667c72c09b80af2e7d854a92c8
JacobADyerGarr <Jdyer.garrison@gmail.com>
2021-02-19 14:40:48 +0000

    Update example.sh for MLP

--- example.sh
+++ example.sh
@@ -8,7 +8,8 @@
 ###########
 
 # Activate Conda Environment [assuming your Miniconda installation is in your root directory]
-source ~/miniconda3/bin/activate nlu
+# J - commented out for use on MLP cluster
+# source ~/miniconda3/bin/activate nlu
 
 # Define a location for all your experiments to save
 ROOT=$(git rev-parse --show-toplevel)
