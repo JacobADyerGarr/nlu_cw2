f6e3193400e5848ffd3960ade0bc838cd78e4c8f
JacobADyerGarr <Jdyer.garrison@gmail.com>
2021-03-01 14:55:37 +0000

    Update example.sh

--- example.sh
+++ example.sh
@@ -16,7 +16,7 @@ RESULTS_ROOT="${ROOT}/results"
 mkdir -p ${RESULTS_ROOT}
 
 ### NAME YOUR EXPERIMENT HERE ##
-EXP_NAME="lexical_tiny"
+EXP_NAME="lexical_full"
 ################################
 
 ## Local variables for current experiment
@@ -30,7 +30,7 @@ mkdir -p ${EXP_ROOT}
 python train.py --save-dir "${EXP_ROOT}" \
                 --log-file "${EXP_ROOT}/log.out"  \
                 --data "${DATA_DIR}" \
-                --train-on-tiny \
+                --decoder-use-lexical_model True \
                 ### ADDITIONAL ARGUMENTS HERE ###
 
 ## Prediction step
